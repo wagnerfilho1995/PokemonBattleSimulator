8e66bf71804adef14e2ac72904c0d925f8a2e633
wagnerfilho1995 <wagnerfilho1995@gmail.com>
2017-03-05 22:14:39 -0300

    Simple changes
    - Addition Pokemon (Pikachu);
    - Transport battle code to a Method;
    - Simplificated some repeated lines of codes;

--- src/main/java/me/pavva/PokemonBattleSimulator/App.java
+++ src/main/java/me/pavva/PokemonBattleSimulator/App.java
@@ -130,135 +130,53 @@ class Pokemon { //Pokemon Object Constructors and get-set methods
 
 public class App { //Contains public static void main(String[] args) and the main game loop within
 
-    public static void main(String[] args) throws IllegalArgumentException {
-
-        final String ANSI_RESET = "\u001B[0m"; //ANSI Color Codes
-        final String ANSI_BLACK = "\u001B[30m";
-        final String ANSI_RED = "\u001B[31m";
-        final String ANSI_GREEN = "\u001B[32m";
-        final String ANSI_YELLOW = "\u001B[33m";
-        final String ANSI_BLUE = "\u001B[34m";
-        final String ANSI_PURPLE = "\u001B[35m";
-        final String ANSI_CYAN = "\u001B[36m";
-        final String ANSI_WHITE = "\u001B[37m";
-
-        final String ANSI_BLACK_BACKGROUND = "\u001B[40m"; //ANSI Background Color Codes
-        final String ANSI_RED_BACKGROUND = "\u001B[41m";
-        final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
-        final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
-        final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
-        final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
-        final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
-        final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
-
-        Scanner s = new Scanner(System.in); //Used for User Input
+	public static void battle(Pokemon yourPoke, Pokemon opponentPoke,  Pokemon[] poke){
+		
+		Scanner s = new Scanner(System.in); //Used for User Input
         Random rand = new Random(); //Random Numbers
-
-        Pokemon Chimchar = new Pokemon("Chimchar", "fire", 120, 0, "Flamethrower", "Flame Charge", "Tackle", "Skull Bash");
-        Pokemon Bulbasaur = new Pokemon("Bulbasaur", "grass", 120, 0, "Energy Ball", "Solar Ball", "Vine Whip", "Tackle");
-        Pokemon Squirtle = new Pokemon("Squirtle", "water", 120, 0, "Aqua Jet", "Hydro Cannon", "Skull Bash", "Crunch");
-
-        Pokemon[] poke = new Pokemon[]{Chimchar, Bulbasaur, Squirtle};
-        
-        for (int i = 0; i < poke.length; i++) {
-
-            System.out.println(poke[i].getName() + " is of type " + poke[i].getType() + ". It has " + poke[i].getHealth()
-                               + " health points and it knows " + poke[i].getMove1() + ", " + poke[i].getMove2() + ", "
-                               + poke[i].getMove3() + ", " + poke[i].getMove4() + ".");
-
-        }
-
-        System.out.println("\nChoose the number that corresponds to the Pokemon you would like to play with in the list above.");
-        int yourPick = s.nextInt();
-        int opponentPick = rand.nextInt(poke.length);
-
-        Pokemon yourPoke = new Pokemon();
-        Pokemon opponentPoke = new Pokemon();
-
-        if (yourPick == 1) {
-
-            yourPoke = poke[yourPick - 1];
-            opponentPoke = poke[opponentPick];
-            System.out.println("You have selected " + yourPoke.getName());
-            System.out.println("Your opponent has selected " + opponentPoke.getName());
-
-        } else if (yourPick == 2) {
-
-            yourPoke = poke[yourPick - 1];
-            opponentPoke = poke[opponentPick];
-            System.out.println("You have selected " + yourPoke.getName());
-            System.out.println("Your opponent has selected " + opponentPoke.getName());
-
-        } else if (yourPick == 3) {
-
-            yourPoke = poke[yourPick - 1];
-            opponentPoke = poke[opponentPick];
-            System.out.println("You have selected " + yourPoke.getName());
-            System.out.println("Your opponent has selected " + opponentPoke.getName());
-
-        } else if (yourPick > poke.length || yourPick < poke.length) {
-            s.close();
-            throw new IllegalArgumentException("\n\n" + ANSI_RESET + ANSI_RED + "Please pick an integer between 1 and " + poke.length + ", inclusive.\n"
-                                               + ANSI_RESET);
-        }
-
+    	
+		int yourPick;
+	    int opponentPick;
+		
         while (yourPoke.getHealth() > 0.0 && opponentPoke.getHealth() > 0.0) {
-            
-            System.out.println(yourPoke.getName() + " has " + yourPoke.getHealth() + " health left.");
+        	
+        	System.out.println("********* BATTLE *********");
+             
+            System.out.println("Your " + yourPoke.getName() + " has " + yourPoke.getHealth() + " health left.");
             System.out.println("The foe's " + opponentPoke.getName() + " has " + opponentPoke.getHealth() + " health left.\n");
-
-            System.out.println(yourPoke.getName() + " knows " + yourPoke.getMove1() + ", " + yourPoke.getMove2() + ", "
-                               + yourPoke.getMove3() + ", " + yourPoke.getMove4());
+            System.out.println("Your " + yourPoke.getName() + " knows");
+            System.out.println("1 - " + yourPoke.getMove1() + "		;	2 - " + yourPoke.getMove2());
+            System.out.println("3 - " + yourPoke.getMove3() + "		;	4 - " + yourPoke.getMove4());
             System.out.println("Type the number corresponding to the move you would like " + yourPoke.getName() + " to use. Select '5' to switch out to a different Pokemon.");
             yourPoke.setUsedMove(s.nextInt());
 
             if (yourPoke.getUsedMove() == 1) {
 
                 opponentPoke.setHealth(opponentPoke.getHealth() - 20);
-                System.out.println(yourPoke.getName() + " used " + yourPoke.getMove1());
+                System.out.println(yourPoke.getName() + " used " + yourPoke.getMove1() + " !");
 
             } else if (yourPoke.getUsedMove() == 2) {
 
                 opponentPoke.setHealth(opponentPoke.getHealth() - 18);
-                System.out.println(yourPoke.getName() + " used " + yourPoke.getMove2());
+                System.out.println(yourPoke.getName() + " used " + yourPoke.getMove2() + " !");
 
             } else if (yourPoke.getUsedMove() == 3) {
 
                 opponentPoke.setHealth(opponentPoke.getHealth() - 25);
                 yourPoke.setHealth(yourPoke.getHealth() - 5);
-                System.out.println(yourPoke.getName() + " used " + yourPoke.getMove3());
+                System.out.println(yourPoke.getName() + " used " + yourPoke.getMove3() + " !");
 
             } else if (yourPoke.getUsedMove() == 4) {
 
                 opponentPoke.setHealth(opponentPoke.getHealth() - 30);
                 yourPoke.setHealth(yourPoke.getHealth() - 10);
-                System.out.println(yourPoke.getName() + " used " + yourPoke.getMove4());
+                System.out.println(yourPoke.getName() + " used " + yourPoke.getMove4() + " !");
                 
-            } else if (yourPoke.getUsedMove() == 5) {
-                
-                for (int j = 0; j < poke.length; j++) {
-
-                    System.out.println(poke[j].getName() + " is of type " + poke[j].getType() + ". It has " + poke[j].getHealth()
-                        + " health points and it knows " + poke[j].getMove1() + ", " + poke[j].getMove2() + ", "
-                        + poke[j].getMove3() + ", " + poke[j].getMove4() + ".");
-                    
-                }
-                
-                System.out.println("\nChoose the number that corresponds to the Pokemon you would like to play with in the list above.");
-                yourPick = s.nextInt();
-                
-                if (yourPick > poke.length) {
-                    s.close();
-                    throw new IllegalArgumentException(ANSI_RED + "Please pick a number between 1 and " + poke.length + "." + ANSI_RESET);
-                } else {
-                    yourPoke = poke[yourPick - 1];
-                }
-
-                
-            } else {
+            }                
+            else {
                 
                 s.close();
-                throw new IllegalArgumentException(ANSI_RED + "Please enter a number between 1 and 4, inclusive" + ANSI_RESET);
+                throw new IllegalArgumentException("Please enter a number between 1 and 4, inclusive");
 
             }
             
@@ -340,9 +258,93 @@ public class App { //Contains public static void main(String[] args) and the mai
 
             }
 
-
         }
+		
+	}
+	
+    public static void main(String[] args) throws IllegalArgumentException {
 
+        final String ANSI_RESET = "\u001B[0m"; //ANSI Color Codes
+        final String ANSI_BLACK = "\u001B[30m";
+        final String ANSI_RED = "\u001B[31m";
+        final String ANSI_GREEN = "\u001B[32m";
+        final String ANSI_YELLOW = "\u001B[33m";
+        final String ANSI_BLUE = "\u001B[34m";
+        final String ANSI_PURPLE = "\u001B[35m";
+        final String ANSI_CYAN = "\u001B[36m";
+        final String ANSI_WHITE = "\u001B[37m";
+
+        final String ANSI_BLACK_BACKGROUND = "\u001B[40m"; //ANSI Background Color Codes
+        final String ANSI_RED_BACKGROUND = "\u001B[41m";
+        final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+        final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+        final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+        final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+        final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+        final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
+        Scanner s = new Scanner(System.in); //Used for User Input
+        Random rand = new Random(); //Random Numbers
+        
+        int play = 1;
+        
+        while(play == 1){
+        
+	        int aux = 0;
+	        
+	        Pokemon Chimchar = new Pokemon("Chimchar", "fire", 120, 0, "Flamethrower", "Flame Charge", "Tackle", "Skull Bash");
+	        Pokemon Bulbasaur = new Pokemon("Bulbasaur", "grass", 120, 0, "Energy Ball", "Solar Ball", "Vine Whip", "Tackle");
+	        Pokemon Squirtle = new Pokemon("Squirtle", "water", 120, 0, "Aqua Jet", "Hydro Cannon", "Skull Bash", "Crunch");
+	        Pokemon Pikachu = new Pokemon("Pikachu", "eletric", 120, 0, "Thunder", "ThunderShock", "Slam", "Quick Attack");
+	
+	        Pokemon[] poke = new Pokemon[]{Chimchar, Bulbasaur, Squirtle, Pikachu};
+	        
+	        Pokemon yourPoke = new Pokemon();
+	        Pokemon opponentPoke = new Pokemon();
+	        
+	        while(aux == 0){
+		
+	        	System.out.println("******** POKEBALLS *********");
+		        
+		        for (int i = 0; i < poke.length; i++) {
+		
+		            System.out.println((i+1) + " - " + poke[i].getName() + " is of type " + poke[i].getType() + ". It has " + poke[i].getHealth()
+		                               + " health points and it knows " + poke[i].getMove1() + ", " + poke[i].getMove2() + ", "
+		                               + poke[i].getMove3() + ", " + poke[i].getMove4() + ".");
+		
+		        }
+		
+		        System.out.println("\nChoose the number that corresponds to the Pokemon you would like to play with in the list above.");
+		        int yourPick = s.nextInt();
+		        int opponentPick = rand.nextInt(poke.length);
+		    
+		        yourPoke = poke[yourPick - 1];
+		        opponentPoke = poke[opponentPick];
+		       
+		        if (yourPick > poke.length || yourPick < 0) {
+		            s.close();
+		            throw new IllegalArgumentException("\n\n" + ANSI_RESET + ANSI_RED + "Please pick an integer between 1 and " + poke.length + ", inclusive.\n"
+		                                               + ANSI_RESET);
+		        }
+		        
+		        System.out.println("You have selected " + yourPoke.getName() + "!");
+		        System.out.println("Your opponent has selected " + opponentPoke.getName() + "!");
+		        
+		        System.out.println("\nWanna change?");
+		        System.out.println("0 - Yes");
+		        System.out.println("1 - No");
+		        aux = s.nextInt();
+	        }
+	        
+	        battle(yourPoke, opponentPoke, poke);
+	 
+	        System.out.println("\nWanna play again?");
+	        System.out.println("0 - No");
+	        System.out.println("1 - Yes");
+	        play = s.nextInt();
+	    
+        }
+        
         s.close();
 
     }
